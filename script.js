d910e2936659ab10511ec001cf6d0f858c9576cd
Artem Erusov <erusovav@gmail.com>
2025-03-01 10:09:35 +0300

    Made the main functionality

--- script.js
+++ script.js
@@ -1,39 +1,61 @@
 const ps = {
-    lowercase: "abcdefghijklmnopqrstuvwxyz",
-    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    symbols: "!@#$%^&*()-_=+[]{}|;:',.<>?/`~",
-    numbers: "1234567890"
-  };
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  symbols: "!@#$%^&*()-_=+[]{}|;:',.<>?/`~",
+  numbers: "1234567890",
+};
 
-  const passwordLength = document.querySelector('.custom-range');
-  const passwordDetails = document.querySelector('.pass-lenght .detail span');
-  const passwordindicator = document.querySelector('.password-indicator');
+const passwordLength = document.querySelector(".custom-range");
+const passwordDetails = document.querySelector(".pass-lenght .detail span");
+const passwordindicator = document.querySelector(".password-indicator");
+const generatorButton = document.querySelector(".generate-btn");
+const passwordOutput = document.querySelector(".password-output");
+const uppercase = document.getElementById("uppercase");
+const numbers = document.getElementById("numbers");
+const symbols = document.getElementById("symbols");
+const copyButton = document.querySelector(".copy");
 
-  const updatePasswordIndicator = (num) => {
-    passwordDetails.textContent = num;
-    if (num <= 20){
-      passwordindicator.classList.add('weak');
-      passwordindicator.classList.remove('strong');
-      passwordindicator.classList.remove('medium');
-    } else if(num <= 28){
-      passwordindicator.classList.remove('strong');
-      passwordindicator.classList.remove('weak');
-      passwordindicator.classList.add('medium');
-    } else {
-      passwordindicator.classList.remove('medium');
-      passwordindicator.classList.remove('weak');
-      passwordindicator.classList.add('strong');
+
+const randomInteger = (min, max) =>
+  Math.floor(min + Math.random() * (max + 1 - min));
+
+const copyPassword = () =>{
+  navigator.clipboard.writeText(passwordOutput.value)
+}
+
+
+const updatePasswordIndicator = (num) => {
+  passwordDetails.textContent = num;
+  if (num <= 20) {
+    passwordindicator.classList.add("weak");
+    passwordindicator.classList.remove("strong", "medium");
+  } else if (num <= 28) {
+    passwordindicator.classList.remove("strong", "weak");
+    passwordindicator.classList.add("medium");
+  } else {
+    passwordindicator.classList.remove("medium", "weak");
+    passwordindicator.classList.add("strong");
+  }
+};
+
+
+const generatePassword = () => { 
+  let passString = ps.lowercase;
+  if (uppercase.checked) passString += ps.uppercase;
+  if (numbers.checked) passString += ps.numbers;
+  if (symbols.checked) passString += ps.symbols;
+
+  const lenght = +passwordLength.value;
+  updatePasswordIndicator(lenght);  
+    let randomPassword = "";
+    for (let i = 0; i < lenght; i++){
+      randomPassword += passString[randomInteger(0, passString.length - 1)]
     }
-  }
+    passwordOutput.value = randomPassword;
+    console.log(passString)
+    console.log(ps.uppercase)
+};
 
-
-  const generatePassword = () => {
-    const lenght = +passwordLength.value;
-    console.log(typeof(lenght), lenght)
-    updatePasswordIndicator(lenght)
-    
-  }
-
-
-
-  passwordLength.oninput = generatePassword;
+passwordLength.oninput = generatePassword;
+copyButton.onclick = copyPassword;
+generatorButton.onclick = generatePassword;
